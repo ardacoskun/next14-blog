54cb129b3d66153fd0938a686b52751c94700a7c
Arda Coşkun <m.ardacoskun@gmail.com>
2024-11-10 15:11:26 +0300

    tailwind css start

--- app/about/projects/page.jsx
+++ app/about/projects/page.jsx
@@ -6,7 +6,7 @@ export const dynamic = "force-dynamic";
 
 const Page = async () => {
   return (
-    <div className="p-20">
+    <div>
       <h1 className="mb-8">Projects</h1>
       <div className="mb-8">Hello this is the list</div>
       <Suspense fallback={<ProjectListLoading />}>
