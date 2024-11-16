1fa19a8f94bff995cb37b5acc38b194fd1a4d0d8
Arda Coşkun <m.ardacoskun@gmail.com>
2024-11-17 01:51:35 +0300

    notFound pages added

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,4 +1,10 @@
+import { notFound } from "next/navigation";
+
 const Page = ({ params }) => {
+  if (!["first", "second"].includes(params.slug)) {
+    notFound();
+  }
+
   return <div>Hello! {params.slug}</div>;
 };
 
