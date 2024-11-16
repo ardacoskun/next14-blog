9aa1e10cdcb7793ac8af2fabe7b43f52b762d415
Arda Coşkun <m.ardacoskun@gmail.com>
2024-11-16 23:12:58 +0300

    projects page refactor

--- app/about/projects/page.jsx
+++ app/about/projects/page.jsx
@@ -1,21 +1,15 @@
-const Page = async () => {
-  const response = await fetch("http://localhost:3001/repos", {
-    cache: "no-store",
-  });
-  const repos = await response.json();
+import { Suspense } from "react";
+import ProjectList from "@/components/projects/ProjectList";
+import ProjectListLoading from "@/components/projects/ProjectListLoading";
 
+const Page = async () => {
   return (
     <div className="p-20">
       <h1 className="mb-8 text-xl">Projects</h1>
-      <ul>
-        {repos.map((repo) => (
-          <li key={repo.id} className="mb-4">
-            <div>{repo.title}</div>
-            <div>{repo.description}</div>
-            <div>{repo.stargazers_count}</div>
-          </li>
-        ))}
-      </ul>
+      <div className="mb-8">Hello, this is the list of my repos!</div>
+      <Suspense fallback={<ProjectListLoading />}>
+        <ProjectList />
+      </Suspense>
     </div>
   );
 };
