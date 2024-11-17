f126b2f433836527d1c6f9d853ffc651794d18c7
Arda Coşkun <m.ardacoskun@gmail.com>
2024-11-17 11:03:36 +0300

    next image component added

--- app/photos/page.jsx
+++ app/photos/page.jsx
@@ -1,20 +1,26 @@
+import dog1 from "./../../public/images/dog1.png";
+import dog2 from "./../../public/images/dog2.png";
+import dog3 from "./../../public/images/dog3.png";
+import dog4 from "./../../public/images/dog4.png";
+import Image from "next/image";
+
+const images = [dog1, dog2, dog3, dog4];
+
 const Page = () => {
   return (
     <div>
       <h1 className="mb-8 text-2xl font-semibold">My Photos</h1>
       <div className="grid grid-cols-2 gap-4">
-        <div className="overflow-hidden h-60">
-          <img src="/images/dog1.png" className="object-cover w-full h-full" />
-        </div>
-        <div className="overflow-hidden h-60">
-          <img src="/images/dog2.png" className="object-cover w-full h-full" />
-        </div>
-        <div className="overflow-hidden h-60">
-          <img src="/images/dog3.png" className="object-cover w-full h-full" />
-        </div>
-        <div className="overflow-hidden h-60">
-          <img src="/images/dog4.png" className="object-cover w-full h-full" />
-        </div>
+        {images.map((item, index) => (
+          <div key={index} className="relative overflow-hidden h-60">
+            <Image
+              fill
+              src={item}
+              className="object-cover w-full h-full"
+              alt="Picture of my dog"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
