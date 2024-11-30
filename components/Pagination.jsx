6d4f7fce1ee83a0adc611b86d1d1c79323ac2d8a
Arda Coşkun <m.ardacoskun@gmail.com>
2024-11-30 13:07:40 +0300

    pagination component improvements

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -1,18 +1,36 @@
 "use client";
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
 
 const Pagination = ({ pageCount }) => {
-  console.log("🚀 ~ Pagination ~ pageCount:", pageCount);
-
-  const pages = [];
-  for (let i = 1; i <= pageCount; i++) {
-    pages.push(i);
-  }
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const currentPage = searchParams.get("page") || "1";
 
   return (
     <ul className="flex justify-center space-x-4 font-mono text-lg">
-      {pages.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
+      {Array.from({ length: pageCount }, (_, i) => {
+        const pageNumber = (i + 1).toString();
+        const params = new URLSearchParams(searchParams);
+        params.set("page", pageNumber);
+
+        const isActive = currentPage === pageNumber;
+
+        return (
+          <li key={pageNumber}>
+            <Link
+              href={`${pathName}?${params.toString()}`}
+              className={`${
+                isActive
+                  ? "underline decoration-gray-400 underline-offset-4 decoration-4"
+                  : ""
+              } text-gray-500 dark:text-gray-400`}
+            >
+              {pageNumber}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
